ea919f7451597e4e602b1ef043245a234403d644
Rashed Arman <rashedarman21@gmail.com>
2023-01-24 21:54:00 +0600

    Create Header component

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from './Header';
 import TodoList from './TodoList';
 
 class TodoContainer extends React.Component {
@@ -25,6 +26,7 @@ class TodoContainer extends React.Component {
   render() {
     return (
       <div className="container">
+        <Header />
         <TodoList todos={this.state.todos} />
       </div>
     );
